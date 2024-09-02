31a2c04682bbec9476a6a0a25e5646c6f2b52272
Michelle379 <michelle379123@gmail.com>
2024-09-02 20:17:48 +0800

    Initial Commit

---
+++ CountMoney/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CountMoney
+//
+//  Created by 王宜婕 on 2024/9/2.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
